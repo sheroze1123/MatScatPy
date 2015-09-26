9550fad44c279942f8b3d9650dae4f26b049ad19
sheroze1123 <mss385@cornell.edu>
2015-09-26 14:34:23 -0400

    dictionaries for each element of elt

--- square_well.py
+++ square_well.py
@@ -1,18 +1,10 @@
 import numpy as np
 import sys
 
-class Element:
-    def __init__(self, a, b, order, Vtype, V):
-        self.a     = a
-        self.b     = b
-        self.order = order
-        self.Vtype = Vtype
-        self.V     = V
-
 def square_well(ab=np.array([-2, -1, 1, 2]), V=np.array([0, -10, 0]), h=1, order=20):
     """Discretize a problem where the potential is given by a piecewise constant.
     
-    Keyword arguments:
+    Input:
       ab    -- breakpoints between regions where the potential is constant
       V     -- potential values on each interval ab(j) to ab(j+1)
       h     -- maximum element size (default 1)
@@ -29,10 +21,12 @@ def square_well(ab=np.array([-2, -1, 1, 2]), V=np.array([0, -10, 0]), h=1, order
         L = ab[j+1] - ab[j]
         nelt = int(np.ceil(L/h))
         for k in range(1, nelt+1):
-            a = ab[j] + L*(k-1)/nelt 
-            b = ab[j] + L*k/nelt
-            el = Element(a, b, order, 'constant', V[j])            
+            el = {}
+            el['a'] = ab[j] + L*(k-1)/nelt 
+            el['b'] = ab[j] + L*k/nelt
+            el['order'] = order
+            el['Vtype'] = 'constant'
+            el['V'] = V[j]
             elt.append(el)
-
     return elt
 
