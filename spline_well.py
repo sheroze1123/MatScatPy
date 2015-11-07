92922b6cc31c05c8f9e25f65187a87134e43f60b
chaitalijoshi <chaitali.catseye@gmail.com>
2015-11-07 00:18:42 -0500

    Initial spline_well

---
+++ spline_well.py
@@ -0,0 +1,31 @@
+import numpy as np
+from scipy import interpolate
+
+
+def spline_well(xx=None, Vx=None, h=1, order=20):
+    """elt = spline_well(xx, Vx, h, order)
+    
+      Discretize a problem where the potential is given by a spline.
+       Input:
+        xx    - interpolation points
+        Vx    - spline values at the points
+        h     - maximum element size (default: 1)
+        order - maximum element order (default: 20)"""
+    if xx is None and Vx is None:
+        Vx = [0,0.5,1,0.5, 0]
+        xx = [-1,-0.5,0,0.5,1]
+
+    elt = []
+    pp = interpolate.splrep(xx, Vx)
+    for j in range(0,len(Vx)):
+        L = xx[-1] - xx[0]
+        nelt = int(np.ceil(L/h))
+        for k in range(1, nelt+1):
+            el = {}
+            el['a'] = xx[0] + L*(k-1)/nelt 
+            el['b'] = xx[0] + L*k/nelt
+            el['order'] = order
+            el['Vtype'] = 'spline'
+            el['V'] = pp
+            elt.append(el)
+    return elt
